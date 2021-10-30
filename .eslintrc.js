31a8704fd05e5e2dd66f4f259ddf50c3dde17d36
princejoogie <princejoogie@gmail.com>
2021-10-30 08:42:04 +0800

    inital commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+};
